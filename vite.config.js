2f2f5e16521903afdd38bf93b2c22179468d037f
Hoshi <62409960+MonsoonR@users.noreply.github.com>
2026-07-12 14:01:15 +0800

    chore: add project configuration

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
+const isUserOrOrganizationSite = repositoryName?.endsWith('.github.io');
+const base =
+  process.env.GITHUB_ACTIONS === 'true' && repositoryName && !isUserOrOrganizationSite
+    ? `/${repositoryName}/`
+    : '/';
+
+export default defineConfig({
+  base,
+  plugins: [react()],
+});
